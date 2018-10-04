9f2d3479925c403673248ed4c1cba3c6fc3a26cb
rietzche <haritzrahman98@gmail.com>
2018-10-04 13:33:45 +0700

    database heroku

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
-$ url  =  parse_url ( getenv ( " DATABASE_URL " ));
-$ host  =  $ url [ " host " ];
-$ username  =  $ url [ " user " ];
-$ password  =  $ url [ " pass " ];
-$ database  =  substr ( $ url [ " path " ], 1 );
+$url  =  parse_url ( getenv ( " DATABASE_URL " ));
+$host  =  $url [ " host " ];
+$username  =  $url [ " user " ];
+$password  =  $url [ " pass " ];
+$database  =  substr ( $url [ " path " ], 1 );
 
 return [
 
