251af4bea9f81a282afa268cc4465503a14ddf9a
rietzche <haritzrahman98@gmail.com>
2018-10-04 13:31:47 +0700

    database heroku

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
-// $ url  =  parse_url ( getenv ( " DATABASE_URL " ));
-// $ host  =  $ url [ " host " ];
-// $ username  =  $ url [ " user " ];
-// $ password  =  $ url [ " pass " ];
-// $ database  =  substr ( $ url [ " path " ], 1 );
+$ url  =  parse_url ( getenv ( " DATABASE_URL " ));
+$ host  =  $ url [ " host " ];
+$ username  =  $ url [ " user " ];
+$ password  =  $ url [ " pass " ];
+$ database  =  substr ( $ url [ " path " ], 1 );
 
 return [
 
@@ -60,16 +60,16 @@ return [
             'engine' => null,
         ],
 
-    //    'pgsql' => array(
-    //         'driver'   => 'pgsql',
-    //         'host'     => $host,
-    //         'database' => $database,
-    //         'username' => $username,
-    //         'password' => $password,
-    //         'charset'  => 'utf8',
-    //         'prefix'   => '',
-    //         'schema'   => 'public',
-    //     ),
+       'pgsql' => array(
+            'driver'   => 'pgsql',
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ),
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
